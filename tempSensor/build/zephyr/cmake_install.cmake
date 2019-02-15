1e959f63cff7eda2d4d7eacfdb6cb0040eb446d7
Mark Grennan <grennama@tcd.ie>
2019-02-15 17:32:19 +0000

    temperature sensor with LED

---
+++ tempSensor/build/zephyr/cmake_install.cmake
@@ -0,0 +1,52 @@
+# Install script for directory: /home/markg/cs7ns2/zephyr
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "TRUE")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("/home/markg/git/iot/team05/CodeBase/tempLED/build/zephyr/arch/cmake_install.cmake")
+  include("/home/markg/git/iot/team05/CodeBase/tempLED/build/zephyr/lib/cmake_install.cmake")
+  include("/home/markg/git/iot/team05/CodeBase/tempLED/build/zephyr/misc/cmake_install.cmake")
+  include("/home/markg/git/iot/team05/CodeBase/tempLED/build/zephyr/soc/arm/cmake_install.cmake")
+  include("/home/markg/git/iot/team05/CodeBase/tempLED/build/zephyr/boards/cmake_install.cmake")
+  include("/home/markg/git/iot/team05/CodeBase/tempLED/build/zephyr/ext/cmake_install.cmake")
+  include("/home/markg/git/iot/team05/CodeBase/tempLED/build/zephyr/subsys/cmake_install.cmake")
+  include("/home/markg/git/iot/team05/CodeBase/tempLED/build/zephyr/drivers/cmake_install.cmake")
+  include("/home/markg/git/iot/team05/CodeBase/tempLED/build/zephyr/tests/cmake_install.cmake")
+  include("/home/markg/git/iot/team05/CodeBase/tempLED/build/zephyr/kernel/cmake_install.cmake")
+  include("/home/markg/git/iot/team05/CodeBase/tempLED/build/zephyr/cmake/flash/cmake_install.cmake")
+  include("/home/markg/git/iot/team05/CodeBase/tempLED/build/zephyr/cmake/usage/cmake_install.cmake")
+  include("/home/markg/git/iot/team05/CodeBase/tempLED/build/zephyr/cmake/reports/cmake_install.cmake")
+
+endif()
+
