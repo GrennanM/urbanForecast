d3c7f447bc57ba1e946f22652fd66e495834933a
Mark Grennan <grennama@tcd.ie>
2019-03-16 22:08:43 +0000

    updated url for tides info

--- backendTwitter/settings2.py
+++ backendTwitter/settings2.py
@@ -6,3 +6,4 @@ CONSUMER_SECRET = 'XXXXXXXXXXXX'
 dublinBayBuoy = ['2320627075'] # DublinBayBuoy ID
 
 myUrl = 'XXXXXXXXXXXX'
+urlTidesInfo =  'https://www.tide-forecast.com/locations/Dublin-Ireland/tides/latest'
