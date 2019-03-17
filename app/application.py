262c5789103455e0c743937f552b5d8cea910623
Mark Grennan <grennama@tcd.ie>
2019-03-17 17:43:58 +0000

    added method to get average water temperature from db

--- app/application.py
+++ app/application.py
@@ -1,9 +1,12 @@
+from flask import Flask, render_template
 from firebase import firebase
 from datetime import datetime, timedelta
 from settings import *
 
+# get readings from Firebase
 firebase = firebase.FirebaseApplication(myUrl, None)
 results = firebase.get('', None)
+
 
 def getCurrentWeather():
     """returns: last 10 weather readings from firebase"""
@@ -19,13 +22,29 @@ def getCurrentWeather():
     # return weatherList[-10:]
     return weatherList
 
-from flask import Flask, render_template
+
+def getCurrentWaterTemperature():
+    """returns average of last 10 water temperature readings from db"""
+
+    # get water temperature readings
+    waterTempList = [w['waterTemp'] for w in results.values()]
+
+    result = 0
+    for i in range(10):
+        result += waterTempList[-i]
+
+    averageWaterTemp = round(result / 10, 1)
+
+    return averageWaterTemp
+
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def helloWorld():
-    return render_template("home.html", weatherList=getCurrentWeather())
+    return render_template("home.html", weatherList=getCurrentWeather(),
+                           waterTemp=getCurrentWaterTemperature())
 
 
 # export FLASK_APP=application.py
@@ -40,3 +59,4 @@ def helloWorld():
 # display star rating
 # graph wave height
 # insert image
+# update table in real time (js)
