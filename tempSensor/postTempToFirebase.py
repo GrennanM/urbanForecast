a24a2b6f8be27e11366e6b34f439430f9c2b20f1
Mark Grennan <grennama@tcd.ie>
2019-03-17 19:54:35 +0000

    changed post method to use firebase library, added rounded time and timestamp, posts to new branch on firebase (/temp)

--- tempSensor/postTempToFirebase.py
+++ tempSensor/postTempToFirebase.py
@@ -1,31 +1,48 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 
-"""Reads temperature from serial port and posts to Firebase"""
+"""Reads temperature from serial port every 5 minutes and posts to Firebase"""
 
 import serial
 import re
-from datetime import datetime
-import json
-import requests
-import dataset
+from datetime import datetime, timedelta
+from settings import *
+from firebase import firebase
+import time
 
 
-myUrl = 'XXXXXXXXXXX'
+def roundTime(dateTimeString):
+    """rounds a datetime string to nearest 20 minutes,
+    input: dateTimeString, output: time as string (hours:minutes)"""
 
+    dateTimeObj = datetime.strptime(dateTimeString, '%d/%m/%Y %H:%M:%S')
+
+    # round to nearest 20 minute
+    discard = timedelta(minutes=dateTimeObj.minute % 20)
+    dateTimeObj -= discard
+    if discard > timedelta(minutes=10):
+        dateTimeObj += timedelta(minutes=20)
+
+    result = dateTimeObj.strftime('%H:%M')
+
+    return result
+
+
+# define serial port
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
-# read temperature from serial port and convert to float
-temp = str(ser.readline())
-tempFloat = float(re.search(r'[0-9][0-9].[0-9][0-9]', temp)[0])
+while True:
+    # read temperature from serial port and convert to float
+    temp = str(ser.readline())
+    tempFloat = round(float(re.search(r'[0-9][0-9].[0-9][0-9]', temp)[0]), 1)
 
-# add timestamp
-currentTime = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+    # add timestamp
+    currentTime = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+    roundedTime = roundTime(currentTime)
 
-# post to Firebase
-json = {'Temperature': tempFloat, 'dateTime': currentTime}
-requests.post(url=myUrl, json=json)
+    # post temp to Firebase
+    firebase = firebase.FirebaseApplication(myUrl, None)
+    toPost = {"temp": tempFloat, 'dateTime': currentTime,
+              'roundedTime': roundedTime}
+    firebase.post('/airTemp', toPost)
 
-# add to local db
-db = dataset.connect('sqlite:///temperature.db')
-table = db['tempTable']
-table.insert(json)
+    time.sleep(300)
