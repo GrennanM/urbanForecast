ff69f38513f0a71f0a6148de33b76d0d15ac16ab
Mark Grennan <grennama@tcd.ie>
2019-03-19 09:20:19 +0000

    removed old folder

--- backendTwitter/settings2.py
+++
@@ -1,9 +0,0 @@
-ACCESS_TOKEN = 'XXXXXXXXXXXX'
-ACCESS_SECRET = 'XXXXXXXXXXXX'
-CONSUMER_KEY = 'XXXXXXXXXXXX'
-CONSUMER_SECRET = 'XXXXXXXXXXXX'
-
-dublinBayBuoy = ['2320627075'] # DublinBayBuoy ID
-
-myUrl = 'XXXXXXXXXXXX'
-urlTidesInfo =  'https://www.tide-forecast.com/locations/Dublin-Ireland/tides/latest'
