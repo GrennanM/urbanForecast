c8865bff41d7b7a8429f18e8fcc39491f1869754
Mark Grennan <grennama@tcd.ie>
2019-03-21 14:54:59 +0000

    added comments

--- backend/getHistoricalTweets.py
+++ backend/getHistoricalTweets.py
@@ -23,14 +23,15 @@ table = db['historical']
 # get historical tweets from timeline
 for status in tweepy.Cursor(api.user_timeline, id=DUBLINBAYBUOY_ID).items():
 
-    tweet = status.text # record text of tweet
+    tweet = status.text
     parsedTweet = parseTweet(tweet)
-    # add tweet to db
+
+    # add tweet to local db
     try:
         table.insert(parsedTweet)
     except Exception as e:
         print("Failed to post to db: ", e)
 
 
-# print number of rows in db
-print(len(table))
+# # print number of rows in db
+# print(len(table))
