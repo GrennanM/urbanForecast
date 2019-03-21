8239a7658d5aa55044961e308756aa3f60a1aa5a
Mark Grennan <grennama@tcd.ie>
2019-03-21 11:23:28 +0000

    removed method to retrieve historical weather readings as no longer necessary

--- app/application.py
+++ app/application.py
@@ -7,21 +7,6 @@ from settings import *
 firebase = firebase.FirebaseApplication(myUrl, None)
 weatherResults = firebase.get('/weather', None)
 
-def getCurrentWeather():
-    """returns: last 10 weather readings from firebase"""
-    weatherList = []
-    currentWeather = []
-    for weatherReading in weatherResults.values():
-        currentWeather.extend([weatherReading['roundedTime'], weatherReading['waveHeight'],
-                               weatherReading['compassDir'], weatherReading['avgWind'],
-                               weatherReading['tideHeight'], weatherReading['starRating']])
-        weatherList.append(currentWeather)
-        currentWeather = []
-
-    return weatherList[-1:]
-    # return weatherList
-
-
 def getCurrentWaterTemperature():
     """returns average of last 10 water temperature readings from db"""
 
@@ -56,21 +41,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def helloWorld():
-    return render_template("home.html", weatherList=getCurrentWeather(),
-                           waterTemp=getCurrentWaterTemperature(),
+    return render_template("home.html",  waterTemp=getCurrentWaterTemperature(),
                            airTemp=getCurrentAirTemperature())
-
-
-# export FLASK_APP=application.py
-# export FLASK_ENV=development
-
-# to do:
-# parse and round time to nearest 20
-# display last 20 readings
-# display wind direction
-# display tide
-# display water and air temp in nav bar
-# display star rating
-# graph wave height
-# insert image
-# update table in real time (js)
